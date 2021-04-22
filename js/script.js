6d360832eeb577442d8e4a3acdbc7ec9dcf5b364
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-04-22 16:07:52 +0200

    add scaffholding and imported vue

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Descrizione:
+ * Rifare l'esercizio della to do list (come fatto in precedenza con jQuery) ma questa
+ * volta con Vue, un po come fatto assieme in classe:
+ * - stampare in pagina un item per ogni elemento contenuto in un array di todo
+ * - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto
+ * invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+ * - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla
+ * lista
+ */
+
+console.log('JS Works!');
+console.log('Vue', Vue);
